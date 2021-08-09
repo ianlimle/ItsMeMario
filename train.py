fa1860789e6ee0af9636a52b0a2641da42ac9a81
ianlimle <ianlimle@hotmail.com>
2021-08-09 15:14:53 +0800

    [Update] add argparser for training script

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 import random, datetime
 from pathlib import Path
+import argparse
 
-import gym
 import gym_super_mario_bros
 from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 from nes_py.wrappers import JoypadSpace
@@ -14,6 +14,21 @@ from agent import Mario
 from wrappers import ResizeObservation, SkipFrame
 
 import config
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--memory", type=int, default=config.MEMORY,
+	help="number of past values in the replay memory - contains state, next state, action, reward")
+ap.add_argument("-exp", "--exploration_rate_decay", type=float, default=config.EXPLORATION_RATE_DECAY,
+	help="percentage by which the model reduces the exploration rate")
+ap.add_argument("-l", "--learn_every", type=int, default=config.LEARN_EVERY,
+	help="number of steps (frames) taken before the policy network trains from the memory")
+ap.add_argument("-s", "--sync_every", type=int, default=config.SYNC_EVERY,
+	help="number of steps taken before the policy network and target network sync weights")
+ap.add_argument("-eps", "--episodes", type=int, default=config.EPISODES,
+	help="number of episodes to train the Mario agent for")
+args = vars(ap.parse_args())
+
 
 # init Super Mario environment
 env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
@@ -43,17 +58,24 @@ env.reset()
 save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 save_dir.mkdir(parents=True)
 
-checkpoint = None # eg. Path('checkpoints/2021-08-01T18-25-27/mario.chkpt')
+checkpoint = None
 print("[INFO] State dimensions:", (4, 84, 84))
 print("[INFO] Action dimensions:", env.action_space.n)
 
 # init the Mario agent
-mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
+mario = Mario(state_dim=(4, 84, 84), 
+            action_dim=env.action_space.n, 
+            save_dir=save_dir, 
+            memory=args["memory"], 
+            exploration_rate_decay=args["exploration_rate_decay"],
+            learn_every=args["learn_every"],
+            sync_every=args["sync_every"],           
+            checkpoint=checkpoint)
 
 # init the logger
 logger = MetricLogger(save_dir)
 # init number of episodes to train
-episodes = config.EPISODES
+episodes = args["episodes"]
 
 ### train the model EPISODE no. of times by playing the game
 for e in range(episodes):
